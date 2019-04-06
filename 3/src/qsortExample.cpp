89fc93f194a35af08ffb9bddf2bc5494757e8d91
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-04-06 09:10:46 +0300

    add qsortExample

---
+++ 3/src/qsortExample.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int compareFunc(const void * voidA, const void * voidB);
+
+int main() {
+    const int ARRAY_SIZE = 10;
+    int intArray[ARRAY_SIZE] = {87, 28, 100, 78, 84, 98, 75, 70, 81, 68};
+    qsort(intArray, ARRAY_SIZE, sizeof(int), compareFunc);
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        std::cout << intArray[i] << ", ";
+}
+
+
+int compareFunc(const void * voidA, const void * voidB) {
+    int * intA = (int *)(voidA);
+    int * intB = (int *)(voidB);
+    return *intA - *intB;
+}
