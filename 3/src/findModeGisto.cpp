178d6ea5dd017c6169f031506be6d6327c29d17b
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-04-06 11:24:47 +0300

    add findMode gistogram

---
+++ 3/src/findModeGisto.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int compareFunc(const void * voidA, const void * voidB);
+
+int main() {
+    const int ARRAY_SIZE = 12;
+    int surveyData[ARRAY_SIZE] = {4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10};
+
+    const int MAX_RESPONSE = 10;
+    int histogram[MAX_RESPONSE];
+    for (int i = 0; i < MAX_RESPONSE; i++) {
+        histogram[i] = 0;
+    }
+    for (int i =0; i < ARRAY_SIZE; i++) {
+        histogram[surveyData[i] - 1]++;
+    }
+
+    int mostFrequent = 0;
+    for (int i = 1; i < MAX_RESPONSE; i++) {
+        if (histogram[i] > histogram[mostFrequent]) mostFrequent = i;
+    }
+    mostFrequent++;
+    std::cout << mostFrequent;
+}
