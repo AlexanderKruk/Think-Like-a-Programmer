66445fb5157268c3239058ecf1a02eda5760120e
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-04-22 14:20:37 +0300

    fix 6_3

--- 6/src/tasks/6_2.cpp
+++ 6/src/tasks/6_2.cpp
@@ -5,7 +5,7 @@ bool findParityIterative(int array[], int size);
 bool findParityRecursive(int array[], int size);
 
 int main() {
-    int array[] = {1, 0, 1, 0, 0, 1, 1};
+    int array[] = {0, 1, 0, 0, 1, 1};
     cout << findParityIterative(array, 6) << "\n";
     cout << findParityRecursive(array, 6);
 }
@@ -20,9 +20,9 @@ bool findParityIterative(int array[], int size){
 }
 
 bool findParityRecursive(int array[], int size){
-    if(size == 0) return 0;
-    int current = array[0];
+    if(size == 1) return !(bool)array[0];
+    bool current = (bool)array[size - 1];
     bool count = findParityRecursive(array, size - 1);
-    if(current == 1) return !count;
+    if(current) return !count;
     else return count;
 }
